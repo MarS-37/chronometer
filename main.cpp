138a4f7a3ad616575f4e9c9c960d8dd885e7851b
Mars-37 <master_sbo@mail.ru>
2024-04-26 20:33:04 +0300

    classroom move: module TimeFinish()

--- main.cpp
+++ main.cpp
@@ -13,56 +13,92 @@
 class Chronometr
 {
 public:
-    Chronometr() : started(false) {};
-    void TimeStart()
-    {
-        started = true;
-        // get the current time
-        start_time = std::chrono::steady_clock::now();
-        auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        // structure for storing locale time
-        std::tm start_tm;
-        // get the locale time
-        localtime_s(&start_tm, &now);
-        // that's milliseconds
-        std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-            start_time.time_since_epoch()
-        );
-        // output the start time in the format HH:MM:SS.mmm
-        std::cout << "Start_time: ";
-        // format and display hours, minutes and seconds
-        std::cout << std::put_time(&start_tm, "%H:%M:%S");
-        // output milliseconds
-        std::cout << "." << std::setw(3) << std::setfill('0') << ms.count() % 1000 << std::endl;
-    }
+	Chronometr() : started(false) {};
+	// method locks the start
+	void TimeStart()
+	{
+		started = true;
+		// get the current time
+		start_time = std::chrono::steady_clock::now();
+		auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		// structure for storing locale time
+		std::tm start_tm;
+		// get the locale time
+		localtime_s(&start_tm, &now);
+
+		// get milliseconds
+		std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+			start_time.time_since_epoch()
+			);
+
+		// output the start time in the format HH:MM:SS.mmm
+		std::cout << "Start_time: ";
+		// format and display hours, minutes and seconds
+		std::cout << std::put_time(&start_tm, "%H:%M:%S");
+		// output milliseconds
+		std::cout << "." << std::setw(3) << std::setfill('0') << ms.count() % 1000 << "\n\n";
+	}
+	// method to end the time countdown
+	void TimeFinish()
+	{
+		// if timing is started
+		if (started) {
+			// get the current time
+			auto finish_time = std::chrono::steady_clock::now();
+			auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+			// structure for storing locale time
+			std::tm finish_tm;
+			// get the locale time
+			localtime_s(&finish_tm, &now);
+
+			// get milliseconds
+			std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+				finish_time.time_since_epoch()
+			);
+
+			// output the finish time in the format HH:MM:SS.mmm
+			std::cout << "Finish time: ";
+			// format and display hours, minutesand seconds
+			std::cout << std::put_time(&finish_tm, "%H:%M:%S");
+			std::cout << "." << std::setw(3) << std::setfill('0') << ms.count() % 1000 << std::endl;
+		}
+		else {
+			// if the time countdown has not started, 
+			// display an error message
+			std::cout << "Timer has not started.\n";
+		}
+	}
 
 private:
-    bool started;
-    std::chrono::time_point<std::chrono::steady_clock> start_time;
+	bool started;
+	std::chrono::time_point<std::chrono::steady_clock> start_time;
 };
 
 
 int main() {
-    Chronometr timer;
-    timer.TimeStart();
-
-
-    
-    //// ==> payload	
-    //long long int l{ 1 };
-    //for (long long int i = 1; i < 20'000'000'000; ++i) {
-    //    // payload
-    //    if (l >= 10'000'000'000) {
-    //        std::cout << i << std::endl;
-    //        l = 1;
-    //    }
-    //    else ++l;
-    //}
-    //std::cout << "\n";
-    //// ============
+	Chronometr timer;
+	timer.TimeStart();
 
 
 
+	// ==> payload	
+	long long int l{ 0 };
+	for (long long int i = 1; i < 100'000'000'001; ++i) {
+	    // payload
+		++l;
+	    if (l >= 10'000'000'000) {
+	        std::cout << i << std::endl;
+	        l = 0;
+	    }
+	}
+	std::cout << "\n";
+	// ============
 
-    return 0;
+
+
+	timer.TimeFinish();
+
+
+
+	return 0;
 }
